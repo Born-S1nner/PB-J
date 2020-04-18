e27cd8dcaa1866d0f5b56e3cf329bd8c6f7db532
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2020-04-18 14:41:04 -0400

    created PB&J.css

---
+++ PB&J.js
@@ -0,0 +1,28 @@
+$(function () {
+//whole
+    $(".section li").on('click', function() {
+        var $box = $(this).closest('.view-panel')
+//li system
+        $box.find(".section li.active").removeClass('active')
+        $(this).addClass('active');
+//panel itself
+        var panelToShow = $(this).attr('rel');
+
+        $box.find('.panel,active').slideUp(300, showPanel);
+
+        function showPanel() {
+            $(this).removeClass('.active');
+
+            $('#'+panelToShow).slideDown(300, function() {
+                $(this).addClass('active')
+            });
+        };
+
+    });
+});
+
+$(function () {
+    $('li').on('click', function() {
+        $('.context-active').hide()
+    })
+});
