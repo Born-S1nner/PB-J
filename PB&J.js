10ee8faa3022910b009b9572349a53ce7179b113
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2020-04-20 18:45:08 -0400

    updated PB&J.js 5

--- PB&J.js
+++ PB&J.js
@@ -26,3 +26,41 @@ $(function () {
         $('.context').hide()
     })
 });
+
+$(function() {
+
+    //configuration
+    let width = 720;
+    let animationSpeed = 1000;
+    let pause = 3000;
+    let currentSlide = 1;
+
+    //cache DOM
+    let $slider = $('#flex-slider');
+    let $slideContainer = $slider.find('.slides');
+    let $slides = $slideContainer.find('.slide');
+
+    let interval;
+
+    function startSlider(){
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                currentSlide++;
+                if(currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+
+    function stopSlider() {
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+    startSlider();
+    //listen to mouseEnter and pause
+    //resume at mouse leave
+});
